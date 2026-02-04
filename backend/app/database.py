4568dee47fa1d44263f1a3a9c1f1f8758dd40211
RAJENDRA KUMAR GUPTA <158123880+Rajendradevloperofficial@users.noreply.github.com>
2026-02-04 08:48:27 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,8 +4,16 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
 
 Base = declarative_base()
 
